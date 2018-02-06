774d7825d0f2ca16716c9d8b2d74ed885eabbce3
liudingdan <dingdan.liu@yzamc.com>
2018-02-06 17:34:52 +0800

    使用element

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -1,23 +1,88 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>
+        <div class="line"></div>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <span class="left logo">
+            <img src="../static/img/logo.png" />
+          </span>
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">社区</el-menu-item>
+          <!-- <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-submenu> -->
+          <el-menu-item index="3">
+            <a href="#" target="_blank">会员</a>
+          </el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+      <el-footer id="footer">
+        <p id="p1">
+          <span>
+            本网站版权归站长所有。
+          </span>
+          <br />
+        </p>
+        <p id="p2">
+          <span>
+            当使用本站时，代表您已接受了本站的使用条款和隐私条款。版权所有，保留一切权利。
+          </span>
+        </p>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: "App"
+  };
+
+  import "element-ui/lib/theme-chalk/index.css";
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .logo {
+    float: left;
+    padding-left: 10px;
+    padding-right: 50px;
+  }
+
+  #footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    /* Set the fixed height of the footer here */
+    height: 60px;
+    background-color: #f5f5f5;
+  }
+
+  #footer #p1 {
+    margin-top: 10px;
+    font-size: 12px;
+    text-align: center;
+  }
+
+  #footer #p2 {
+    margin-top: 10px;
+    font-size: 12px;
+    text-align: center;
+  }
+
 </style>
--- webpage/src/pages/home/Index.vue
+++ webpage/src/pages/home/Index.vue
@@ -1,18 +1,73 @@
-<template>
-  <p>124234</p>
+<template> 
+<el-row :gutter="20">
+  <el-col :span="6">
+    <div class="grid-content bg-purple">
+              <div class="tab_1_img">
+            <img src="../static/img/common_65_icon.jpg" />
+        </div>
+        <div class="tabl_1_title">
+            <ul>
+                <li style="font-size:16px;font-weight:600"><a href="#">Red Net</a></li>
+                <li>帖子:{{500}}</li>
+            </ul>
+        </div>
+    </div>
+    </el-col>
+  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+</el-row>
+
 </template>
+
+<style>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+
+  .bg-purple {
+    background: #d3dce6;
+  }
+
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+
+</style>
+
+
 <script>
-export default {
-  name: "HomeIndex"
-};
+  export default {
+    name: "HomeIndex"
+  };
 
-// axios.get('http://localhost:63155/Account/Login?userName=123&pwd=22')
-// .then(function(respone){
-//   console.log(response);
-// })
-// .catch(function(error){
-//   console.log(error);
-// });
-
+  // axios.get('http://localhost:63155/Account/Login?userName=123&pwd=22')
+  // .then(function(respone){
+  //   console.log(response);
+  // })
+  // .catch(function(error){
+  //   console.log(error);
+  // });
 
 </script>
