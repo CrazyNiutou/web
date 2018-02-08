8dd5780636dfc9ca763da3fbd14c6f76ff7de658
CrazyNiuTou <552463805@qq.com>
2018-02-08 22:05:47 +0800

    1

---
+++ webpage/src/pages/content/Articles.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="tet"> 
+  <ul>
+    <li  v-for="(item,index) in items" :key="index">
+      {{item.message}}
+    </li>
+  </ul>
+</div>
+</template>
+<script>
+import Vue from "vue";
+export default {
+  name: "ContentArticles"
+}; 
+var temp = new Vue({
+  el: "#tet",
+  data: {
+    items: []
+  },
+  mounted() {
+    axios.get("https://free-api.heweather.com/s6/weather/now?location=CN101010100&key=227e8c46911e473ea5b00590643b1299")
+    .then(response=>{this.items= response.data.results })
+  }
+});
+</script>
---
+++ webpage/src/pages/member/Members.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>444</p>
+</template>
+<script>
+export default {
+  name:"MemberLink",
+}
+</script>
