63f2cf2ffe825cf57cccf74e451b109b780ce584
liudingdan <dingdan.liu@yzamc.com>
2018-02-28 17:32:31 +0800

    添加图片

--- webpage/src/pages/member/Members.vue
+++ webpage/src/pages/member/Members.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <p class="title"> 会员充值 </p>
-    <div>
+    <div class="content">
       <p>
-        目前本网内容只对会员开放。
-      </p>
-       <p >
-         会员充值方式步骤如下：
+        目前本网内容只对会员开放。 会员充值方式步骤如下：
+        <br/> 1、
+        <span style="color:red">青铜</span>
+        <br/> 期限：半年
+        <br/> 权限：期限内全站所有版块无限下载、免回复、免广告
+        <br/> 价格：115元
+        <br/> 2、
+        <span style="color:red">白银</span>
+        <br/> 期限：一年
+        <br/> 权限：期限内全站所有版块无限下载、免回复、免广告
+        <br/> 价格：215元
+        <br/> 3、
+        <span style="color:red">黄金</span>
+        <br/> 期限：永久
+        <br/> 权限：永久全站所有版块无限下载、免回复、免广告
+        <br/> 价格：415元
+        <br/>
       </p>
     </div>
   </div>
@@ -14,7 +27,19 @@
 
 <style>
   div .title {
-    font-size: 18px;
+    text-align: left;
+    font-size: 21px;
+    font-weight: bold;
+    line-height: 1.5;
+    padding: 7px 25px;
+    background-color: green;
+    color: #fff;
+    border: 1px solid #09a800;
+  }
+
+  div .content {
+    text-align: left;
+    border: 1px solid #d0d3d0;
   }
 
 </style>
@@ -22,7 +47,7 @@
 
 <script>
   export default {
-    name: "MemberLink",
-  }
+    name: "MemberLink"
+  };
 
 </script>
